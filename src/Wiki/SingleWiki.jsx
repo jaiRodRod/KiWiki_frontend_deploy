72e3b1251e78cc4f57f853b147323b5cc8186e5a
Adrián Fernández Vera <adrianfeve7@gmail.com>
2024-12-30 14:25:30 +0100

    Otro

--- src/Wiki/SingleWiki.jsx
+++ src/Wiki/SingleWiki.jsx
@@ -29,7 +29,7 @@ function SingleWiki({ item }) {
 
     const deleteHandler = async(event) =>{
       event.stopPropagation()
-      await axios.delete(apiEndpoint.api + 'wikis/' + item._id + '/')
+      await axios.delete(apiEndpoint.api + '/wikis/' + item._id + '/')
 
     }
 
